989e2155e909008df363c675cde41b0295d41e60
morgansson <17188509+morgansson@users.noreply.github.com>
2023-04-29 17:26:18 -0300

    Added new options and improved functionality
    Added support for optional mouse scrolling, draggable, snap on drag, and mobile native mode. Fixed bugs, and improved overall functionality.

--- README.md
+++ README.md
@@ -4,58 +4,68 @@ A Google like jQuery scrolling carousel plugin.
 # Why
 Because there wasn't any jQuery carousel plugin with the functionality that I needed.
 
+# Default options
+```
+{
+  scrollAmount: 'viewport',
+  mouseScrolling: true,
+  draggable: true,
+  snapOnDrag: true,
+  mobileNative: true,
+}
+```
 # How to use
 ```html
 <html>
 <head>
   <link href="jquery.gScrollingCarousel.css" rel="stylesheet" />
-    <style>
-        body{
-            padding: 5px 13px;
-        }
-        .g-scrolling-carousel .items{
-            padding: 5px 0;
-        }
-        .g-scrolling-carousel .items a{
-            display: inline-block; /* notice the comments between inline-block items */
-            margin-right: 10px;
-            width: 70px;
-            height: 50px;
-            box-shadow: 0 0 5px #000;
-            text-align: center;
-        }
-    </style>
+  <style>
+      body{
+          padding: 5px 13px;
+      }
+      .g-scrolling-carousel {
+        width:607px;
+        margin: 0 auto;
+      }
+      .g-scrolling-carousel .items > *{
+        min-height:96px;
+        margin-right:10px;
+      }
+      .g-scrolling-carousel .items a:last-child{
+        margin-right:0;
+      }
+  </style>
 </head>
 <body>
-<div class="g-scrolling-carousel">
-  <div class="items">
-    <a href="http://example.com">Item #1</a><!--
-    --><a href="http://example.com">Item #2</a><!--
-    --><a href="http://example.com">Item #3</a><!--
-    --><a href="http://example.com">Item #4</a><!--
-    --><a href="http://example.com">Item #5</a><!--
-    --><a href="http://example.com">Item #6</a><!--
-    --><a href="http://example.com">Item #7</a><!--
-    --><a href="http://example.com">Item #8</a><!--
-    --><a href="http://example.com">Item #9</a><!--
-    --><a href="http://example.com">Item #10</a><!--
-    --><a href="http://example.com">Item #11</a><!--
-    --><a href="http://example.com">Item #12</a><!--
-    --><a href="http://example.com">Item #13</a><!--
-    --><a href="http://example.com">Item #14</a><!--
-    --><a href="http://example.com">Item #15</a><!--
-    --><a href="http://example.com">Item #16</a><!--
-    --><a href="http://example.com">Item #17</a><!--
-  --></div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script> 
-<script src="jquery.gScrollingCarousel.js"></script> 
-<script>
-  $(document).ready(function(){
-    $(".g-scrolling-carousel .items").gScrollingCarousel();
-  });
-</script>
+  <div class="g-scrolling-carousel carousel">
+    <div class="items">
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/1/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/2/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/3/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/4/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/5/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/6/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/7/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/8/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/9/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/10/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/11/144/96"></a>
+      <a href="http://example.com" target="_blank"><img src="https://picsum.photos/seed/12/144/96"></a>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script> 
+  <script src="jquery.gScrollingCarousel.js"></script> 
+  <script>
+    $(document).ready(function(){
+      $(".g-scrolling-carousel .items").gScrollingCarousel({
+        scrollAmount: 'viewport',
+        mouseScrolling: true,
+        draggable: true,
+        snapOnDrag: true,
+        mobileNative: true,
+      });
+    });
+  </script>
 </body>
 </html>
 ```
--- jquery.gScrollingCarousel.js
+++ jquery.gScrollingCarousel.js
@@ -1,147 +1,167 @@
 (function($) {
     $.fn.extend({
-        gScrollingCarousel: function(options) {
+      gScrollingCarousel: function(options) {
+        var defaults = {
+          mouseScrolling: true,
+          scrollAmount: 'viewport',
+          draggable: true,
+          snapOnDrag: true,
+          mobileNative: true,
+        };
+  
+        options = $.extend(defaults, options);
 
-            var defaults = {
-                scrolling: true,
-                amount: false
-            };
+        var supportsTouch = false;
+        if ('ontouchstart' in window){
+            supportsTouch = true;
+            } else if(window.navigator.msPointerEnabled) {
+            supportsTouch = true;
+            } else if ('ontouchstart' in document.documentElement) {
+            supportsTouch = true;
+        }
 
-            options = $.extend(defaults, options);
-
-            var supportsTouch = false;
-
-            if ('ontouchstart' in window){
-                supportsTouch = true;
-                } else if(window.navigator.msPointerEnabled) {
-                supportsTouch = true;
-                } else if ('ontouchstart' in document.documentElement) {
-                supportsTouch = true;
+        function getMaxScrollLeft(carousel){
+          return carousel.get(0).scrollWidth - carousel.get(0).clientWidth;
+        }
+        function updateNavigationVisibility(carousel) {
+          var maxScrollLeft = getMaxScrollLeft(carousel);
+          var left = carousel.scrollLeft();
+          carousel.data("leftElem").css("display", left == 0 ? "none" : "inline-block");
+          carousel.data("rightElem").css("display", left >= maxScrollLeft ? "none" : "inline-block");
+        }
+  
+        function handleClickNavigation(carousel, direction) {
+          var firstElemWidth = carousel.data("firstElemWidth");
+          var firstElemMargin = carousel.data("firstElemMargin"); 
+          var currentScrollLeft = carousel.scrollLeft();
+          var itemsPerViewport = Math.floor((carousel.width() - firstElemWidth + firstElemMargin) / firstElemWidth) + 1;
+          var amountToScroll = options.scrollAmount === 'viewport' ? itemsPerViewport * firstElemWidth : firstElemWidth;
+          var scrollAmount = currentScrollLeft % firstElemWidth;
+          var newScrollPosition;
+          if (direction === 1) {
+            if (scrollAmount === 0) {
+              newScrollPosition = currentScrollLeft + amountToScroll;
+            } else {
+              var partiallyVisibleItemWidth = firstElemWidth - scrollAmount;
+              newScrollPosition = currentScrollLeft + (amountToScroll + partiallyVisibleItemWidth);
+            }
+          } else {
+            if (scrollAmount === 0) {
+              newScrollPosition = currentScrollLeft - amountToScroll;
+            } else {
+              var partiallyVisibleItemWidth = firstElemWidth - scrollAmount;
+              newScrollPosition = currentScrollLeft - (amountToScroll - partiallyVisibleItemWidth);
+            }
+          }
+  
+          carousel.stop(true).animate({ scrollLeft: newScrollPosition }, 200, function() {
+            if(newScrollPosition <= getMaxScrollLeft(carousel)){
+              var snapIndex = Math.round(carousel.scrollLeft() / carousel.data("firstElemWidth"));
+              var snapPosition = snapIndex * carousel.data("firstElemWidth");
+              carousel.scrollLeft(snapPosition);
             }
 
-            if (!supportsTouch){
-                var x,left,down,newX,oldX,maxScrollLeft,am,amX,amL,leftElem,rightElem,currx,items,element,elements;
-                element = $(this);
-                if(options.amount == false) {
-                    amount = element.children(":first").outerWidth(true);
-                }else{
-                    amount = options.amount;
-                }
-                leftElem = $('<span />').addClass('jc-left').html('<svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>');
-                rightElem = $('<span />').addClass('jc-right').html('<svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>');
-                element.parent().append(leftElem).append(rightElem);
+            updateNavigationVisibility(carousel);
+          });
+        }
+  
+  
+        function getFirstElemWidth(carousel) {
+          var firstElem = carousel.children(":first");
+          var marginRight = parseInt(firstElem.css("marginRight"));
+          return firstElem.width() + marginRight;
+        }
+  
+        function getFirstElemMargin(carousel) {
+          var firstElem = carousel.children(":first");
+          return parseInt(firstElem.css("marginRight"));
+        }
+  
 
-                maxScrollLeft = element.get(0).scrollWidth - element.get(0).clientWidth;
-                left = element.scrollLeft();
-                if(maxScrollLeft == left) {
-                    rightElem.hide();
+        return this.each(function() {
+          var carousel = $(this);
+          if (!supportsTouch || !options.mobileNative){
+            var leftElem = $('<span />').addClass('jc-left').html('<svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>');
+            var rightElem = $('<span />').addClass('jc-right').html('<svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>');
+    
+            carousel.parent().append(leftElem).append(rightElem);
+            carousel.data("leftElem", leftElem);
+            carousel.data("rightElem", rightElem);
+            carousel.data("firstElemWidth", getFirstElemWidth(carousel));
+            carousel.data("firstElemMargin",getFirstElemMargin(carousel));
+    
+            leftElem.on("click", function() {
+              handleClickNavigation(carousel, -1);
+            });
+    
+            rightElem.on("click", function() {
+              handleClickNavigation(carousel, 1);
+            });
+    
+            updateNavigationVisibility(carousel);
+
+            if (options.mouseScrolling) {
+              var firstElemWidth = carousel.data("firstElemWidth");
+              var scrollAmount;
+              carousel.on("wheel", function(event) {
+                event.preventDefault();
+                carousel.scrollLeft (scrollAmount);
+                var deltaY = event.originalEvent.deltaY;
+                var direction = deltaY > 0 ? 1 : -1;
+                if(direction==1){
+                  scrollAmount = $(this).scrollLeft() + firstElemWidth;
                 } else {
-                    rightElem.show();
+                  scrollAmount = $(this).scrollLeft() - firstElemWidth;
                 }
-                if(left == 0) {
-                    leftElem.hide();
-                } else {
-                    leftElem.show();
-                }
+                carousel.stop(true).animate({ scrollLeft: scrollAmount }, 100, function() {
+                  updateNavigationVisibility(carousel);
+                });
+              });
+            }
 
-                if(options.scrolling){
-                    element.bind("DOMMouseScroll mousewheel", function (event) {    
-                            var oEvent = event.originalEvent, 
-                            direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta, 
-                            position = element.scrollLeft();
-                        position += direction > 0 ? -amount : amount;
-
-                        $(this).scrollLeft(position);
-                        event.preventDefault();
-                        maxScrollLeft = element.get(0).scrollWidth - element.get(0).clientWidth;
-                        left = element.scrollLeft();
-                        if(maxScrollLeft == left) {
-                            rightElem.fadeOut(200);
-                        } else {
-                            rightElem.fadeIn(200);
-                        }
-                        if(left == 0) {
-                            leftElem.fadeOut(200);
-                        } else {
-                            leftElem.fadeIn(200);
-                        }
-
-                    });
-                }
-                element.bind("mousedown", function(e){
-                    e.preventDefault();
-                    down = true;
-                    x = e.pageX;
-                    left = $(this).scrollLeft();
-                }).bind("mousemove", function(e){
-                    if(down){
-                        if(e.pageX != x){
-                            element.addClass("nonclick");
-                            newX = e.pageX;
-                            oldX = element.scrollLeft();
-                            element.scrollLeft(left-newX+x);  
-                            maxScrollLeft = element.get(0).scrollWidth - element.get(0).clientWidth;
-                            if(maxScrollLeft == oldX) {
-                                rightElem.fadeOut(200);
-                            } else {
-                                rightElem.fadeIn(200);
-                            }
-                            if(oldX == 0) {
-                                leftElem.fadeOut(200);
-                            } else {
-                                leftElem.fadeIn(200);
-                            }
-                        }
-                    } else {
-                        element.removeClass("nonclick");
+            if (options.draggable) {
+              var down,startX,startScrollLeft,newScrollPosition,isDragging;
+              carousel.on("mousedown.gScrollingCarousel", function(e) {
+                e.preventDefault();
+                startX = e.pageX;
+                startScrollLeft = carousel.scrollLeft();
+      
+                carousel.on("mousemove.gScrollingCarousel", function(e) {
+                    if(e.pageX != startX){
+                        isDragging = true;
+                        var deltaX = e.pageX - startX;
+                        newScrollPosition = startScrollLeft - deltaX
+                        carousel.scrollLeft(newScrollPosition);
                     }
                 });
-                rightElem.bind("click", function(e){
-                  leftElem.fadeIn(200);
-                  items = $(this).siblings(".items");
-                  currx = items.scrollLeft();
-                  amX = parseInt($(this).parent().width() / amount); // cantidad de elementos x viewport
-                  am = (amX * amount) - amount;
-                  maxScrollLeft = items.get(0).scrollWidth - items.get(0).clientWidth;
-                  if(currx+am >= maxScrollLeft) $(this).fadeOut(200);
-                  items.animate( { scrollLeft: '+='+am }, 200);
-                });
-                leftElem.bind("click", function(e){
-                  rightElem.fadeIn(200);
-                  items = $(this).siblings(".items");
-                  currx = items.scrollLeft();
-                  amX = parseInt($(this).parent().width() / amount); // cantidad de elementos x viewport
-                  am = (amX * amount) - amount;
-                  if(currx-am <= 0) $(this).fadeOut(200);
-                  items.animate( { scrollLeft: '-='+am }, 200);
-                });
-                $(window).on('resize', function(){
-                    element.each( function(){
-                        elements = $(this);
-                        maxScrollLeft = elements.get(0).scrollWidth - elements.get(0).clientWidth;
-                        left = elements.scrollLeft();
-                        if(maxScrollLeft == left) {
-                            rightElem.fadeOut(200);
-                        } else {
-                            rightElem.fadeIn(200);
-                        }
-                        if(left == 0) {
-                            leftElem.fadeOut(200);
-                        } else {
-                            leftElem.fadeIn(200);
-                        }
+      
+                $(document).on("mouseup.gScrollingCarousel", function() {
+                  carousel.off("mousemove.gScrollingCarousel");
+                  $(document).off("mouseup.gScrollingCarousel");
+                  if (options.snapOnDrag && newScrollPosition < getMaxScrollLeft(carousel)) {
+                    var snapIndex = Math.round(carousel.scrollLeft() / carousel.data("firstElemWidth"));
+                    var snapPosition = snapIndex * carousel.data("firstElemWidth");
+                    carousel.stop(true).animate({ scrollLeft: snapPosition }, 200, function() {
+                      updateNavigationVisibility(carousel);
                     });
+                  } else {
+                    updateNavigationVisibility(carousel);
+                  }
                 });
-                $(document).on("mouseup mousedown click", ".nonclick a", function(e){  //prevent clicking while moving
+              });
+              carousel.on("mouseup mousedown click", function(e){  //prevent clicking while moving
+                if(isDragging) {
                   e.preventDefault();
-                });
-                $(document).on("mouseup", function(e){ //globally remove nonclicks onmouseup
-                    setTimeout(function(){
-                        element.removeClass("nonclick");
-                        down=false;
-                    },1);
-                });
+                  setTimeout(function(){
+                    isDragging = false;
+                  },1);
+                }
+              });
             }
-        }
+          } else {
+            carousel.css("overflow-x","scroll")
+          }
+        });
+      }
     });
-})(jQuery);
+  })(jQuery);
